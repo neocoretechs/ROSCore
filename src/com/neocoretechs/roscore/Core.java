aed4d38b8da060fa480afae5f25c9a05b047c1ec
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2014-08-15 16:36:55 -0700

    ROSCore initial

---
+++ src/com/neocoretechs/roscore/Core.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.neocoretechs.roscore;
+
+import java.util.concurrent.TimeUnit;
+
+import org.ros.RosCore;
+import org.ros.node.DefaultNodeMainExecutor;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+
+/**
+ * Start the ROS core and bind to port 50000
+ * @author jg
+ *
+ */
+public class Core {
+	private static Core instance = null;
+	protected RosCore rosCore;
+	protected NodeConfiguration nodeConfiguration;
+	protected NodeMainExecutor nodeMainExecutor;
+	
+	public static Core getInstance() {
+		if( instance == null ) {
+			instance = new Core();
+		}
+		return instance;
+	}
+	private Core() {}
+	
+	public RosCore getRosCore() {
+		return rosCore;
+	}
+	public NodeConfiguration getNodeConfiguration() {
+		return nodeConfiguration;
+	}
+	public NodeMainExecutor getNodeMainExecutor() {
+		return nodeMainExecutor;
+	}
+
+
+	public void setUp() throws InterruptedException {
+		    rosCore = RosCore.newPublic(50000); // bind port 50000
+		    rosCore.start();
+		    assert(rosCore.awaitStart(1, TimeUnit.SECONDS));
+		    nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+		    nodeConfiguration = NodeConfiguration.newPublic("localhost", rosCore.getUri());
+		    System.out.println("ROS Core Uri:"+rosCore.getUri());
+	 }
+
+	 public void setUp(String bindHost) throws InterruptedException {
+	    rosCore = RosCore.newPublic(bindHost, 50000); // bind port 50000
+	    rosCore.start();
+	    assert(rosCore.awaitStart(1, TimeUnit.SECONDS));
+	    nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
+	    nodeConfiguration = NodeConfiguration.newPublic("localhost", rosCore.getUri());
+	    System.out.println("ROS Core Uri:"+rosCore.getUri());
+	 }
+	 
+	 public void tearDown() {
+		  nodeMainExecutor.shutdown();
+		  rosCore.shutdown();
+	 }
+	 
+	 public static void main(String[] args) throws Exception {
+		 if( args.length < 1) {
+			 System.out.println("Usage java -cp . com.neocoretechs.roscore.Core <host>");
+			 return;
+		 }
+		 Core.getInstance().setUp(args[0]);
+	 }
+}
